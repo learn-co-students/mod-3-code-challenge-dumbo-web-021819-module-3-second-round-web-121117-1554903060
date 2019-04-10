9fcd52f5f512f9e50880437fc37f5cdeca6c79a1
James Dorr <jamesdorr3@gmail.com>
2019-04-10 10:19:50 -0400

    done with deliverables, onto improving

---
+++ src/index.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', e => {
+    // document.addEventListener('click', e => console.log(e.target));
+    fetchAllBeers();
+    listGroupClickListener();
+    editBeerListener()
+});
+
+function fetchAllBeers() {
+    fetch('http://localhost:3000/beers')
+    .then(r => r.json())
+    .then(beersArray => {
+        beersArray.forEach (beer => {
+            const listGroup = document.getElementById('list-group')
+            listGroup.innerHTML += `
+            <li class="list-group-item" data-beer-id="${beer.id}">${beer.name}</li>
+            `
+        })
+    })
+}
+
+function listGroupClickListener() {
+    document.getElementById('list-group').addEventListener('click', e => {
+        if (e.target.tagName === 'LI') {
+            fetch(`http://localhost:3000/beers/${e.target.dataset.beerId}`)
+            .then(r => r.json())
+            .then(postBeerDetailTag)
+        }
+    })
+}
+
+function makeBeerDetailTag(beerObj) {
+    return `
+    <h1>${beerObj.name}</h1>
+    <img src="${beerObj.image_url}">
+    <h3>${beerObj.tagline}</h3>
+    <textarea>${beerObj.description}</textarea>
+    <button id="edit-beer" class="btn btn-info" data-beer-id="${beerObj.id}">
+      Save
+    </button>
+    `
+}
+
+function postBeerDetailTag(beerObj) {
+    document.getElementById('beer-detail').innerHTML = '';
+    document.getElementById('beer-detail').innerHTML += makeBeerDetailTag(beerObj)
+}
+
+function editBeerListener() {
+        document.getElementById('beer-detail').addEventListener('click', e => {
+        if (e.target.id === 'edit-beer') {
+            const beerDetail = e.target.parentElement.querySelector('textarea').value
+            const beerId = e.target.dataset.beerId 
+            updateBeerDetailInDb(beerId, beerDetail)
+        }
+    })
+}
+
+function updateBeerDetailInDb(beerId, beerDetail) {
+    fetch(`http://localhost:3000/beers/${beerId}`, {
+    method: 'PATCH',
+    headers: {'Content-Type': 'application/json', Accept: 'application/json'},
+    body: JSON.stringify({description: beerDetail})})
+}
