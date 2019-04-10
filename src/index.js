3bd9601a909c36cc747264117580091081f7ed15
1-8192 <allegranzia@gmail.com>
2019-04-10 10:18:56 -0400

    finished deliverables

---
+++ src/index.js
@@ -0,0 +1,57 @@
+
+const beerListUl = document.querySelector("#list-group");
+const beerDetailDiv = document.querySelector("#beer-detail");
+
+//beer detail div create
+const createBeerInfo = (beer) => {
+  return `<h1>${beer.name}</h1>
+          <img src="${beer.image_url}">
+          <h3>${beer.tagline}</h3>
+          <textarea>${beer.description}</textarea>
+          <button id="edit-beer" class="btn btn-info" data-id="${beer.id}">
+          Save
+          </button>`
+};
+
+//Populating sidebar with beer names
+fetch("http://localhost:3000/beers")
+  .then((response) => {
+    return response.json();
+  }).then((beers) => {
+    beers.forEach((beer) => {
+      beerListUl.innerHTML += `<li class="list-group-item" data-id="${beer.id}">${beer.name}</li>`
+    })
+  });
+
+//Show details of specific beer on click
+beerListUl.addEventListener('click', (event) => {
+  if (event.target.className = 'LI') {
+    beerDetailDiv.innerHTML = "";
+    fetch(`http://localhost:3000/beers/${event.target.dataset.id}`)
+      .then((response) => {
+        return response.json()
+      }).then((beer) => {
+        beerDetailDiv.innerHTML += createBeerInfo(beer)
+      }).then(() => {
+        //Edit function once beer detail is posted to DOM
+        document.querySelector("#edit-beer").addEventListener('click', (event) => {
+          let desc = event.target.previousElementSibling.value
+          fetch(`http://localhost:3000/beers/${event.target.dataset.id}`, {
+            method: 'PATCH',
+            headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                      },
+            body: JSON.stringify({
+               "description": desc
+            })
+          }).then((response) => {
+            return response.json()
+          }).then((beer) => {
+            beerDetailDiv.innerHTML = "";
+            beerDetailDiv.innerHTML += createBeerInfo(beer)
+          })
+        })
+      })
+  }
+});
